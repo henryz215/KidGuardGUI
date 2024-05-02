23137bedadcbc1896dd819fead1f8237bc62cd86
henryz215 <157532506+henryz215@users.noreply.github.com>
2024-05-02 17:47:56 -0500

    Legend and Indexing Update

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.getElementById("presenceButton").addEventListener("click", function() {
+    var menu = document.querySelector(".presenceMenu");
+    menu.classList.toggle("open");
+  });
+
+  document.getElementById("temperatureButton").addEventListener("click", function() {
+    var menu = document.querySelector(".temperatureMenu");
+    menu.classList.toggle("open");
+  });
+
+  document.getElementById("timerButton").addEventListener("click", function() {
+    var menu = document.querySelector(".timerMenu");
+    menu.classList.toggle("open");
+  });
